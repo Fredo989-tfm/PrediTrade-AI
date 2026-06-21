51c3c9e10b85f1699f9326c4806b5ab99518d0ee
Fredo989-tfm <fredoblong6@gmail.com>
2026-06-21 15:12:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 print("Bienvenue sur PrediTrade AI")
+
+while True:
+    question = input("Pose une question sur un marché financier : ")
+
+    if question.lower() == "quit":
+        break
+
+    print("Analyse PrediTrade AI :")
+    print("Probabilité de hausse : 72%")
+    print("Le marché montre une tendance haussière.")
