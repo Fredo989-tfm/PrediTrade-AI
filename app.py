0b03dd78b33d48454a0c7c9d0c1b034e56b768db
Fredo989-tfm <fredoblong6@gmail.com>
2026-06-24 11:49:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,20 +26,4 @@ if st.button("Analyser"):
     if actif == "BTC":
         ticker = "BTC-USD"
         prob = 78
-        analyse = "Bitcoin montre une tendance haussière soutenue."
-        risque = "Moyen"
-        confiance = "8/10"
-
-    elif actif == "TSLA":
-        ticker = "TSLA"
-        prob = 65
-        analyse = "Tesla reste volatile mais conserve un potentiel intéressant."
-        risque = "Élevé"
-        confiance = "6/10"
-
-    elif actif == "AAPL":
-        ticker = "AAPL"
-        prob = 82
-        analyse = "Apple présente une structure haussière solide."
-        risque = "Faible"
-        confiance = "
+        analyse = "Bitcoin montre une tendance haussière sout
