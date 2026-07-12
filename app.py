d8f83eb47068ee5d21158e0243d55b274a1701b6
Fredo989-tfm <fredoblong6@gmail.com>
2026-07-12 13:36:03 +0100

    Fix trend calculation for market datav9

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ if st.button("Analyser"):
                 except:
                     prix = float(close_data.iloc[-1])
                     
-                tendance = ((close_data.iloc[-30] - close_data.iloc[-1]) / close_data.iloc[-30]) * -100
+                tendance = float(((close_data.iloc[-1] - close_data.iloc[0]) / close_data.iloc[0]) * 100)
 
                 st.subheader("📊 Données réelles du marché")
 
