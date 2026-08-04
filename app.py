b9608455df568edea4581af1b3f28923d6fae69d
Fredo989-tfm <fredoblong6@gmail.com>
2026-08-04 11:00:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,12 +38,25 @@ def activate_premium_user(email):
 
 def page_login():
     st.markdown("""<div style="text-align:center;padding:25px;border-radius:15px;background:linear-gradient(90deg,#0E1117,#1B263B);"><h1 style="color:#00E5FF;">🚀 PrediTrade AI Pro V4.5</h1><h3 style="color:white;">Version Finale 4.5</h3></div>""", unsafe_allow_html=True)
-if st.button(
-    "🔵 Se connecter avec Google",
+    oauth = OAuth2Session(
+    GOOGLE_CLIENT_ID,
+    redirect_uri=REDIRECT_URI,
+    scope=SCOPE,
+)
+
+authorization_url, state = oauth.create_authorization_url(
+    AUTHORIZATION_URL,
+    access_type="offline",
+    prompt="select_account"
+)
+
+st.session_state["oauth_state"] = state
+
+st.link_button(
+    "🔵 Continuer avec Google",
+    authorization_url,
     use_container_width=True,
-    key="google_login"
-):
-    st.info("Connexion Google en cours...")
+)
     tab1, tab2 = st.tabs(["🔐 Connexion", "📝 Inscription"])
     with tab1:
         email = st.text_input("Email", key="login_email")
