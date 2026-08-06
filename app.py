73c413c01ababde14ce0cca15c01163b217c4e36
Fredo989-tfm <fredoblong6@gmail.com>
2026-08-06 15:49:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import json
 from firebase_admin import credentials, messaging
 
 if not firebase_admin._apps:
-    firebase_config = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+    firebase_config = dict(st.secrets["FIREBASE"])
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 def envoyer_notification(token, titre, message):
